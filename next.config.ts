c09fa4f9815f2d6faf6dbbd2d324fa2535a58b8a
Willyam <ngutshinsisili@gmail.com>
2026-02-22 22:11:35 +0100

    commit 22-02-2026

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,15 @@ const nextConfig: NextConfig = {
 
   experimental: {
     serverActions: {
-      allowedOrigins: ["localhost:3000", "localhost:80", "localhost:3001"],
+      allowedOrigins: [
+        "localhost:3000",
+        "localhost:80",
+        "localhost:3001",
+        "mpako.net",
+        "www.mpako.net",
+        "https://www.mpako.net",
+        "https://mpako.net",
+      ],
     },
   },
 };
